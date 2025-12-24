0a2e1d6518cab5b6a80e5c126990d41fa1993fd6
VQ350GeTsome <harrison102636@gmail.com>
2025-12-24 13:21:32 -0500

    Initial commit

---
+++ src/client/java/vq350/getsome/ExtraSnowBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package vq350.getsome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtraSnowBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
